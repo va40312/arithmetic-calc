a72e1bece272fa0d66cbeeae9c4d9484b697995f
va40312 <va40312@yandex.by>
2025-10-01 16:44:08 +0300

    sync progress from mac

--- internal/expression/parser.go
+++ internal/expression/parser.go
@@ -1,5 +1,7 @@
 package expression
 
+import "fmt"
+
 type operatorInfo struct {
 	priority int
 }
@@ -9,6 +11,8 @@ var operators = map[string]operatorInfo{
 	"-": {priority: 1},
 	"*": {priority: 2},
 	"/": {priority: 2},
+	// унарный минус
+	"#": {priority: 3},
 }
 
 // ----- функции для работы со стеком
@@ -30,6 +34,222 @@ func pop(stack []Token) (Token, []Token) {
 	return token, stack
 }
 
+// функция принимает срез токенов и возвращает склееные токены
+// с унарным минусом
+func preprocessUnaryMinus(tokens []Token) ([]Token, error) {
+	var outputTokens []Token
+	for index := 0; index < len(tokens); index++ {
+		token := tokens[index]
+		switch token.TokenType {
+		case Operator:
+			if token.Value != "-" {
+				outputTokens = append(outputTokens, token)
+				continue
+			}
+			if index+1 > len(tokens)-1 {
+				outputTokens = append(outputTokens, token)
+				continue
+			}
+			// сработает проверка i == 0 ниже которая
+			//if index-1 < 0 {
+			//	outputTokens = append(outputTokens, token)
+			//	continue
+			//}
+
+			nextToken := tokens[index+1]
+			if nextToken.TokenType == LeftParen {
+				outputTokens = append(outputTokens,
+					Token{
+						TokenType: LeftParen,
+						Value:     "(",
+					},
+					Token{
+						TokenType: Number,
+						Value:     "0",
+					},
+				)
+			}
+
+			if nextToken.TokenType != Number {
+				outputTokens = append(outputTokens, token)
+				continue
+			}
+
+			if index == 0 {
+				outputTokens = append(outputTokens, Token{
+					TokenType: Number,
+					Value:     "-" + nextToken.Value,
+				})
+				index++ // увеличиваем счетчик на 1 чтобы пропустить число
+				continue
+			}
+
+			previousToken := tokens[index-1]
+
+			if previousToken.TokenType == LeftParen {
+				outputTokens = append(outputTokens, Token{
+					TokenType: Number,
+					Value:     "-" + nextToken.Value,
+				})
+				index++ // увеличиваем счетчик на 1 чтобы пропустить число
+				continue
+			}
+
+			if previousToken.TokenType == Operator {
+				outputTokens = append(outputTokens, Token{
+					TokenType: Number,
+					Value:     "-" + nextToken.Value,
+				})
+				index++ // увеличиваем счетчик на 1 чтобы пропустить число
+				continue
+			}
+			outputTokens = append(outputTokens, token)
+		default:
+			outputTokens = append(outputTokens, token)
+			if token.TokenType == RightParen {
+				outputTokens = append(outputTokens, Token{
+					TokenType: RightParen,
+					Value:     ")",
+				})
+			}
+		}
+	}
+	// обhаботать синтаксическую ошибку если есть унарный минус
+	// но нет числа 2 - (- * 2) после минуса нет числа, это ошибка
+	// !!!!!!!
+	//
+	// -1 - 2
+	// 1 + (-1 + 2)
+	// - -234 стоит еще минус перед выражением
+	// обобщим чтобы минус был перед любым оператором
+	// * -2, / -2, + -2,
+
+	return outputTokens, nil
+}
+
+func validSyntax(tokens []Token) error {
+	for index := 0; index < len(tokens); index++ {
+		token := tokens[index]
+		switch token.TokenType {
+		case Number:
+			if index+1 > len(tokens)-1 {
+				continue
+			}
+			nextToken := tokens[index+1]
+			if nextToken.TokenType == Number || nextToken.TokenType == LeftParen {
+				return fmt.Errorf("[syntax error]: отсутствует оператор между числом %s и %s", token.Value, nextToken.Value)
+			}
+
+			if index == 0 {
+				continue
+			}
+			previousToken := tokens[index-1]
+			if previousToken.TokenType == Number || previousToken.TokenType == RightParen {
+				return fmt.Errorf("[syntax error]: отсутствует оператор между числом `%s` и `%s`", token.Value, previousToken.Value)
+			}
+		case Operator:
+			if index+1 > len(tokens)-1 {
+				return fmt.Errorf("[syntax error]: бинарный оператор `%s` не может быть без правого операнда", token.Value)
+			}
+
+			nextToken := tokens[index+1]
+			isUnaryMinus := token.Value == "-" && nextToken.TokenType == LeftParen
+
+			if isUnaryMinus {
+				// вставить фиктивный ноль
+				continue
+			} else if index == 0 {
+				return fmt.Errorf("[syntax error]: бинарный оператор `%s` не может быть без левого операнда", token.Value)
+			}
+
+			previousToken := tokens[index-1]
+
+			if previousToken.TokenType != Number && previousToken.TokenType != RightParen {
+				return fmt.Errorf("[syntax error]: перед бинарным оператор `%s` должно быть число", token.Value)
+			} else if nextToken.TokenType != Number && previousToken.TokenType != LeftParen {
+				return fmt.Errorf("[syntax error]: после бинарного оператора `%s` должно быть число", token.Value)
+			}
+		}
+	}
+	return nil
+}
+
 func Parser(tokens []Token) ([]Token, error) {
-	return nil, nil
+	var err error
+	// преобразовать унарный минус
+	tokens, err = preprocessUnaryMinus(tokens)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("preprocessUnaryMinus = %+v\n", tokens)
+
+	// проверка синтаксиса на ошибки перед применением алгоритма
+	if err = validSyntax(tokens); err != nil {
+		return nil, err
+	}
+
+	// начало алгоритма сортировочной станции
+	var output []Token
+	var operatorStack []Token
+	var topOperator Token
+
+	for _, token := range tokens {
+		switch token.TokenType {
+		case Number:
+			output = append(output, token)
+
+		case Operator:
+			// проверяем что стек не пустой, что-то в нем есть на вершине
+			// 2 + 2 * 2
+			// 2 * 2 + 2
+			// 10 * 20 / 5 + 3
+			// A + B * C / D - E
+			for len(operatorStack) > 0 && peek(operatorStack).TokenType == Operator {
+				topOperator = peek(operatorStack)
+				topOperatorPriority := operators[topOperator.Value].priority
+				tokenPriority := operators[token.Value].priority
+
+				if tokenPriority <= topOperatorPriority {
+					// убираем верхний элемент в output
+					topOperator, operatorStack = pop(operatorStack)
+					output = append(output, topOperator)
+				} else {
+					// прошли все вершины стека и достигли того, что вытолкнули
+					// все в правильном порядке и приоритете
+					break
+				}
+			}
+			// добавляем в stack оператор новый
+			operatorStack = append(operatorStack, token)
+
+		case LeftParen:
+			operatorStack = append(operatorStack, token)
+
+		case RightParen:
+			// удаляем все пока не встретим открывающую скобку
+			// если нет левой скобочки ошибка
+			for len(operatorStack) > 0 && peek(operatorStack).TokenType != LeftParen {
+				topOperator, operatorStack = pop(operatorStack)
+				output = append(output, topOperator)
+			}
+			// на этом этапе должна остаться LeftParen иначе ошибка синтаксиса
+			if len(operatorStack) == 0 {
+				return nil, fmt.Errorf("[syntax error]: нет открывающей скобки")
+			}
+
+			_, operatorStack = pop(operatorStack)
+		}
+	}
+	// все остатки operatorStack нужно добавить в output после цикла
+	// которые не прошли выталкивание из за порядка они последние
+	// или приоритета
+	for len(operatorStack) > 0 {
+		topOperator, operatorStack = pop(operatorStack)
+		if topOperator.TokenType == LeftParen {
+			return nil, fmt.Errorf("[syntax error]: не найдено закрывающей скобочки для (")
+		}
+		output = append(output, topOperator)
+	}
+	// конец алгоритма сортировочной станции
+	return output, nil
 }
--- internal/expression/parser_test.go
+++ internal/expression/parser_test.go
@@ -13,35 +13,190 @@ func TestParser(t *testing.T) {
 		expectedRPN []Token
 	}{
 		{
-			name:        "Тестирование не валидного оператора `.`",
-			expr:        "2 /. 2",
+			name:        "Тестирование простого примера для приоритета",
+			expr:        "3 + 2 * 2",
+			expectError: false,
+			expectedRPN: []Token{
+				{
+					TokenType: Number,
+					Value:     "3",
+				},
+				{
+					TokenType: Number,
+					Value:     "2",
+				},
+				{
+					TokenType: Number,
+					Value:     "2",
+				},
+				{
+					TokenType: Operator,
+					Value:     "*",
+				},
+				{
+					TokenType: Operator,
+					Value:     "+",
+				},
+			},
+		},
+		{
+			name:        "Тестирование простого примера наоборот для приоритета",
+			expr:        "2 * 3 - 1",
+			expectError: false,
+			expectedRPN: []Token{
+				{
+					TokenType: Number,
+					Value:     "2",
+				},
+				{
+					TokenType: Number,
+					Value:     "3",
+				},
+				{
+					TokenType: Operator,
+					Value:     "*",
+				},
+				{
+					TokenType: Number,
+					Value:     "1",
+				},
+				{
+					TokenType: Operator,
+					Value:     "-",
+				},
+			},
+		},
+		{
+			name:        "Тестирование выражение из нескольких операторов с разным приоритетами",
+			expr:        "10 * 20 / 5 - 3",
+			expectError: false,
+			expectedRPN: []Token{
+				{
+					TokenType: Number,
+					Value:     "10",
+				},
+				{
+					TokenType: Number,
+					Value:     "20",
+				},
+				{
+					TokenType: Operator,
+					Value:     "*",
+				},
+				{
+					TokenType: Number,
+					Value:     "5",
+				},
+				{
+					TokenType: Operator,
+					Value:     "/",
+				},
+				{
+					TokenType: Number,
+					Value:     "3",
+				},
+				{
+					TokenType: Operator,
+					Value:     "-",
+				},
+			},
+		},
+		{
+			name:        "Тестирование синтаксической ошибки c повторами и более одного оператора",
+			expr:        "3 +- 3 ++ 4",
 			expectError: true,
 			expectedRPN: nil,
 		},
-		//{
-		//	name:        "Тестирование синтакстической ошибки c повторами и более одного оператора",
-		//	expr:        "3 +- 3 ++ 4",
-		//	expectError: true,
-		//	expectedRPN: nil,
-		//},
-		//{
-		//	name:        "Тестирование синтакстической ошибки с порядком символа",
-		//	expr:        "- 3 3",
-		//	expectError: true,
-		//	expectedRPN: nil,
-		//},
-		//{
-		//	name:        "Тестирование синтакстической ошибки с пропуском символа",
-		//	expr:        "(  2  (3+4)) * 2",
-		//	expectError: true,
-		//	expectedRPN: nil,
-		//},
-		//{
-		//	name:        "Тестирование синтакстической ошибки с незакрытими скобками",
-		//	expr:        "2 + (3 + 2 + 3",
-		//	expectError: true,
-		//	expectedRPN: nil,
-		//},
+		{
+			name:        "Тестирование синтаксической ошибки с порядком символа",
+			expr:        "- 3 3",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с порядком символа",
+			expr:        "* 3 3",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с пропуском символа",
+			expr:        "(  2  (3+4)) * 2",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с незакрытыми скобками",
+			expr:        "2 + (3 + 2 + 3",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с незакрытыми скобками",
+			expr:        "-2 + 3 - 2 - 3)",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с незакрытыми скобками",
+			expr:        "(-2 -2 ))",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с незакрытыми скобками",
+			expr:        "((-2 -2 )",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование минуса с унарным минусом",
+			expr:        "2 - -5",
+			expectError: false,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с 1 оператором",
+			expr:        "-",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование сложного вложенного минуса со скобками",
+			expr:        "- (-2 -2 - (-2 - ( 3 + 2) )) + 1",
+			expectError: true,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование унарного минуса со скобками",
+			expr:        "-(-2 -2 )",
+			expectError: false,
+			expectedRPN: []Token{
+				{TokenType: Number, Value: "0"},
+				{TokenType: Number, Value: "-2"},
+				{TokenType: Number, Value: "2"},
+				{TokenType: Operator, Value: "-"}, // Это внутренний минус
+				{TokenType: Operator, Value: "-"}, // Это внешний минус
+			},
+		},
+		{
+			name:        "Тестирование унарного минуса со скобками и другим оператором перед ним",
+			expr:        "3 * -(-2 -2 )",
+			expectError: false,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с незакрытыми скобками",
+			expr:        "-(-3 -2 ) - 1",
+			expectError: false,
+			expectedRPN: nil,
+		},
+		{
+			name:        "Тестирование синтаксической ошибки с незакрытыми скобками",
+			expr:        "(-2 -2 )-",
+			expectError: true,
+			expectedRPN: nil,
+		},
 	}
 
 	for _, test := range testCases {
